7e88244d7ec7b5741668822a5a35d157c823b6f9
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-12-27 03:33:03 +0200

    connect with posgresql db and apply constraints to booking model

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'booking_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Mohamed@237',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
