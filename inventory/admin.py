533a392f0e193aba79ce539d1ccfca629f948001
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-12-27 18:28:40 +0200

    make admin panel for RoomType page

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 
-from .models import Property, RoomType, Room
+from .models import PricingRule, Property, RoomImage, RoomType, Room
+
 
 # Register your models here.
+class RoomImageInline(admin.TabularInline):
+    model = RoomImage
+    extra = 1
+
+
+@admin.register(RoomType)
+class RoomTypeAdmin(admin.ModelAdmin):
+    inlines = [RoomImageInline]
+    list_display = [
+        "name",
+        "property",
+        "base_price",
+        "slug",
+        "capacity",
+        "view_type",
+        "is_smoking",
+    ]
+
+
 admin.site.register(Property)
-admin.site.register(RoomType)
+# admin.site.register(RoomType)
 admin.site.register(Room)
+admin.site.register(PricingRule)
