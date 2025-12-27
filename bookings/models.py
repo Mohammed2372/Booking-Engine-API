0e631ad2498dc28d9bfda8df236c0befbb56ab23
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-12-27 03:11:01 +0200

    add inventory (property, room, room type) models and booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.db.models import Model, TextChoices
+from django.conf import settings
+from django.contrib.postgres.constraints import ExclusionConstraint
+from django.contrib.postgres.fields import RangeOperators, DateRangeField
+from django.utils.translation import gettext_lazy as _
+
+from inventory.models import Room
+
 
 # Create your models here.
+class Booking(Model):
+    class Status(TextChoices):
+        PENDING = "PENDING",_('Pending')
+        CONFIRMED = "CONFIRMED",_('Confirmed')
+        CANCELLED = "CANCELLED",_('Canceled')
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='bookings')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='bookings')
+
+    stay_range = DateRangeField()
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        # constraints from the database itself
+        constraints = [
+            ExclusionConstraint(
+                name='exclude_overlapping_bookings',
+                expressions=[
+                    ('stay_range', RangeOperators.OVERLAPS),
+                    ('room', RangeOperators.EQUAL),
+                ],
+                condition=models.Q(status__in=['PENDING', 'CONFIRMED'])
+            ),
+        ]
+
+
+    def __str__(self):
+        return f'Booking {self.id} for {self.room}'
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class Property(Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='room_types')
+    name = models.CharField(max_length=255)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Room(Model):
+    number = models.CharField(max_length=10)
+    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE, related_name='rooms')
+
+    def __str__(self):
+        return f"room number: {self.number}, name: {self.room_type.name}"
