7fd8bba66b56d0afe3bd5aeaa82bc739b2fe24fc
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-12-30 00:32:38 +0200

    customize admin page view for bookings and reviews

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Booking, Review
 
+
 # Register your models here.
-admin.site.register(Booking)
-admin.site.register(Review)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "room", "status", "total_price"]
+    list_filter = ["status"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["id", "booking__user", "booking", "rating", "comment", "created_at"]
+    list_filter = ["created_at", "rating"]
