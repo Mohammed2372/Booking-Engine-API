afb5e575f10a545d7c5097f3b8542e1781c68ad9
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-12-30 21:07:49 +0200

    add phone number field to property model

--- inventory/models.py
+++ inventory/models.py
@@ -12,6 +12,7 @@ class Property(Model):
     description = models.TextField()
     address = models.TextField()
     city = models.CharField(max_length=100, db_index=True)
+    phone_number = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.name
